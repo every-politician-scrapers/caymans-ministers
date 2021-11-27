bd50eaa969917a13b1fbcefff8a28e720b3c16a6
Tony Bowden <tony@tmtm.com>
2021-11-27 17:21:00 +0200

    initial scraper

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -7,17 +7,44 @@ require 'pry'
 class MemberList
   class Member
     def name
-      noko.css('.name').text.tidy
+      Name.new(
+        full: fullname,
+        prefixes: %w[Hon.],
+        suffixes: %w[MP JP Hon. Cert. MBE QC],
+      ).short.delete_suffix(',')
     end
 
     def position
-      noko.css('.position').text.tidy
+      fullposition.split(/ and (?=Minister)/).map(&:tidy)
+    end
+
+    private
+
+    # There are three different sections: Premier+Deputy / Ministers / Ex-Officio
+    # The first has a different layout from the latter two
+
+    def text
+      return noko.text if noko.text =~ /(Minister|Deputy|Attorney)/
+    end
+
+    def parts
+      return unless text
+
+      text.split(/(?=(Minister|Deputy|Attorney))/, 2).map(&:tidy)
+    end
+
+    def fullname
+      parts ? parts.first : noko.text
+    end
+
+    def fullposition
+      parts ? parts.last : noko.parent.next.text.tidy
     end
   end
 
   class Members
     def member_container
-      noko.css('.member')
+      noko.xpath('//span[@style="font-size:16px"]').select { |node| node.text.start_with? 'Hon.' }
     end
   end
 end
